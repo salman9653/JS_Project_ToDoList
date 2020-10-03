7da47521b9cbf4f650ba8c1b917be238663d78fd
slmn9653 <salman9915189734@gmail.com>
2020-10-03 19:35:45 +0530

    initial files

---
+++ index.js
@@ -0,0 +1,52 @@
+var enterButton = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
+function inputLength(){
+	return input.value.length;
+} 
+
+function listLength(){
+	return item.length;
+}
+
+function createListElement() {
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	input.value = ""; //Reset text input field
+
+
+	function crossOut() {
+		li.classList.toggle("done");
+	}
+	li.addEventListener("click",crossOut);
+
+
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("x"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+
+
+	function deleteListItem(){
+		li.classList.add("delete")
+	}
+}
+
+function addListAfterClick(){
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.which ===13) {
+		createListElement();
+	} 
+}
+
+enterButton.addEventListener("click",addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
